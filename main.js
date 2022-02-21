b4a819f1e6e1a12415bbfdd5781174c6c0a610b1
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-21 15:38:43 -0500

    Added a log out of user question as well as a random number generator from between 0 and 7.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let userName = '';
+let userName = 'David';
+let userQuestion = '';
+let randomNumber = Math.floor(Math.random() * 8));
+let eightBall = '';
 
 (userName != null) ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+
+console.log(`${userName} as asked the following question: ${userQuestion}?`);
