729120074a0c086b2793defffe72b603b7418c2e
Matthew Glassman <matthewglassman78@gmail.com>
2022-02-21 18:42:02 -0500

    One spelling correction, change cases to numbers instead of strings, added break statement.

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 let userName = 'David';
-let userQuestion = '';
-let randomNumber = Math.floor(Math.random() * 8));
+let userQuestion = 'Will it rain today?';
+let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = '';
 
 (userName != null) ? console.log(`Hello, ${userName}`) : console.log('Hello!');
 
-console.log(`${userName} as asked the following question: ${userQuestion}?`);
+console.log(`${userName} has asked the following question: ${userQuestion}?`);
+console.log(randomNumber);
 
 switch (randomNumber) {
-    case '0':
+    case 0:
         eightBall = 'Outlook not so good'
         console.log(eightBall);
-    case '1':
+        break;
+    case 1:
         eightBall = 'My sources say no'
         console.log(eightBall);
-    case '2':
+        break;
+    case 2:
         eightBall = 'Reply hazy try again'
         console.log(eightBall);
-    case '3':
+        break;
+    case 3:
         eightBall = 'Do not count on it'
         console.log(eightBall);
-    case '4':
+        break;
+    case 4:
         eightBall = 'Cannot predict now'
         console.log(eightBall);
-    case '5':
+        break;
+    case 5:
         eightBall = 'Signs point to yes'
         console.log(eightBall);
-    case '6':
+        break;
+    case 6:
         eightBall = 'It is decidedly so'
         console.log(eightBall);
-    case '7':
+        break;
+    case 7:
         eightBall = 'It is certain'
         console.log(eightBall);
+        break;
     default:
         console.log('Sorry Magic 8 Ball is out to lunch.');
 }
